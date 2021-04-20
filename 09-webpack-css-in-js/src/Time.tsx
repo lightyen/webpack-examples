2c9afd4c70b902271964bef9de31716d18bf0b8d
lightyen <lightyen0123@gmail.com>
2021-04-20 11:57:26 +0800

    update

--- 09-webpack-css-in-js/src/Time.tsx
+++ 09-webpack-css-in-js/src/Time.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react"
 import { apply, tw } from "twind"
 import { css } from "twind/css"
 
-const center = apply`absolute (right-1/2 transform translate-x-1/2)`
+const center = apply`absolute (right-1/2 transform translate-x-1/2) top-[99%]`
 
 const hidden = apply`hidden opacity-0`
 
@@ -23,10 +23,13 @@ const anime = apply`opacity-90`
 export default function Time({ content, text }: { content: string; text: string }) {
 	const tooltipRef = useRef<HTMLDivElement>(null)
 
-	const delay = 0
+	const delay = 200
 	const h = useRef<number>(0)
+	const k = useRef<number>(0)
 
 	function enter() {
+		window.clearTimeout(k.current)
+		k.current = 0
 		tw(show)
 			.split(" ")
 			.forEach(c => tooltipRef.current?.classList.add(c))
@@ -37,22 +40,26 @@ export default function Time({ content, text }: { content: string; text: string 
 		}, delay)
 	}
 
-	function leave() {
+	function leave(delay: number) {
 		window.clearTimeout(h.current)
 		h.current = 0
-		tw(anime)
-			.split(" ")
-			.forEach(c => tooltipRef.current?.classList.remove(c))
+		k.current = window.setTimeout(() => {
+			tw(anime)
+				.split(" ")
+				.forEach(c => tooltipRef.current?.classList.remove(c))
+		}, delay)
 	}
 
 	return (
 		<span className={tw`relative`}>
-			<span className={tw`hover:(underline cursor-default)`} onPointerEnter={enter} onPointerLeave={leave}>
+			<span className={tw`hover:(cursor-default)`} onPointerEnter={enter} onPointerLeave={() => leave(delay)}>
 				{text}
 			</span>
 			<div
 				ref={tooltipRef}
 				className={tw(tooltip)}
+				onPointerEnter={enter}
+				onPointerLeave={() => leave(0)}
 				onTransitionEnd={() => {
 					// fade out
 					if (h.current == 0) {
